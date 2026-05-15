e12974fa3db03aacc2413cd62d6e16aff65c894b
GitJaouad <jlaourayed@gmail.com>
2026-05-15 01:49:37 +0100

    Solution3

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import os
 class CleanUpFile:
 
     def __init__(self, filename) -> None:
-        self. filename = filename
+        self.filename = filename
 
     def __enter__(self) -> object:
         return self
 
     def __exit__(self, exc_type, exc, tb) -> None:
-            os.remove(self. filename)
+            os.remove(self.filename)
